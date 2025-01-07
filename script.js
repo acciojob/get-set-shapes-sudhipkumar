03f09eae0b5a01338fa79c50cae227e5db603df8
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-07 20:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,11 +14,11 @@ class Rectangle {
 	}
 
 	getArea(){
-		return width * height;
+		console.log(this._width * this._height);
 	}
 }
 
-class Square extends Animal {
+class Square extends Rectangle {
 	constructor(side){
 		this.side=side;
 	}
@@ -28,6 +28,18 @@ class Square extends Animal {
 	}
 }
 
+// const rectangle = new Rectangle(5, 10);
+// console.log(rectangle.width); // Output: 5
+// console.log(rectangle.height); // Output: 10
+// console.log(rectangle.getArea()); // Output: 50
+
+// const square = new Square(7);
+// console.log(square.width); // Output: 7
+// console.log(square.height); // Output: 7
+// console.log(square.getArea()); // Output: 49
+// console.log(square.getPerimeter()); // Output: 28
+
+
 // Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+// window.Rectangle = Rectangle;
+// window.Square = Square;
