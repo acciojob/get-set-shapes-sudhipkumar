c58aa627e838ee2c863101df8b2f5a837a814794
sudhip kumar <123609652+sudhipkumar@users.noreply.github.com>
2025-01-07 20:52:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -42,5 +42,5 @@ class Square extends Rectangle {
 
 
 // Do not change the code below this line
-// window.Rectangle = Rectangle;
-// window.Square = Square;
+window.Rectangle = Rectangle;
+window.Square = Square;
